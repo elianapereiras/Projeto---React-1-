cffb344a1d87b4f32b0579e64b0ca91ec8e2049c
Eliana Pereira da Silva <eliana.pereiras@gmail.com>
2021-11-02 23:50:39 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/elianapereiras" target="_blank" rel="noreferrer">Eliana Pereira</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://www.linkedin.com/in/eliana-pereiras" target="_blank" rel="noreferrer">eliana.pereiras@gmail.com</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
